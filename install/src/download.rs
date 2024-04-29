687e228e590738bc3a39e64c97a7ad26cb35895f
eatradish <sakiiily@aosc.io>
2024-04-29 12:55:41 +0800

    refactor(install): refactor `download.rs`

--- install/src/download.rs
+++ install/src/download.rs
@@ -8,32 +8,70 @@ use reqwest::header::HeaderValue;
 use reqwest::{header::CONTENT_LENGTH, Client};
 use sha2::Digest;
 use sha2::Sha256;
+use snafu::{ensure, OptionExt, ResultExt, Snafu};
 use tokio::io::AsyncWriteExt;
 
-use crate::{DownloadType, InstallError};
+use crate::DownloadType;
+
+#[derive(Debug, Snafu)]
+pub enum DownloadError {
+    #[snafu(display("Download path is not set"))]
+    DownloadPathIsNotSet,
+    #[snafu(display("Local file not found: {}", path.display()))]
+    LocalFileNotFound { path: PathBuf },
+    #[snafu(display("Failed to build download client"))]
+    BuildDownloadClient { source: reqwest::Error },
+    #[snafu(display("Failed to send request"))]
+    SendRequest { source: reqwest::Error },
+    #[snafu(display("Failed to create file: {}", path.display()))]
+    CreateFile {
+        source: std::io::Error,
+        path: PathBuf,
+    },
+    #[snafu(display("Failed to download file: {}", path.display()))]
+    DownloadFile {
+        source: reqwest::Error,
+        path: PathBuf,
+    },
+    #[snafu(display("Failed to write file: {}", path.display()))]
+    WriteFile {
+        source: std::io::Error,
+        path: PathBuf,
+    },
+    #[snafu(display("Checksum mismatch"))]
+    ChecksumMismatch,
+    #[snafu(display("Failed to shutdown file"))]
+    ShutdownFile {
+        source: std::io::Error,
+        path: PathBuf,
+    },
+}
 
 pub fn download_file<F, F2>(
     download_type: &DownloadType,
     progress: Arc<F>,
     velocity: Arc<F2>,
     cancel_install: Arc<AtomicBool>,
-) -> Result<(PathBuf, usize), InstallError>
+) -> Result<(PathBuf, usize), DownloadError>
 where
     F: Fn(f64) + Sync + Send + 'static,
     F2: Fn(usize) + Send + Sync + 'static,
 {
     match download_type {
         DownloadType::Http { url, hash, to_path } => {
-            let to_path = to_path.as_ref().ok_or(InstallError::DownloadPathIsNotSet)?;
+            let to_path = to_path.as_ref().context(DownloadPathIsNotSetSnafu)?;
             Ok((
                 to_path.clone(),
                 http_download_file(url, to_path, hash, progress, velocity, cancel_install)?,
             ))
         }
         DownloadType::File(path) => {
-            if !path.exists() {
-                return Err(InstallError::LocalFileNotFound(path.display().to_string()));
-            }
+            ensure!(
+                path.exists(),
+                LocalFileNotFoundSnafu {
+                    path: path.to_owned()
+                }
+            );
 
             velocity(0);
             progress(100.0);
@@ -52,7 +90,7 @@ fn http_download_file<F, F2>(
     progress: Arc<F>,
     velocity: Arc<F2>,
     cancel_install: Arc<AtomicBool>,
-) -> Result<usize, InstallError>
+) -> Result<usize, DownloadError>
 where
     F: Fn(f64) + Sync + Send + 'static,
     F2: Fn(usize) + Send + Sync + 'static,
@@ -81,18 +119,22 @@ async fn http_download_file_inner<F, F2>(
     progress: Arc<F>,
     velocity: Arc<F2>,
     cancel_install: Arc<AtomicBool>,
-) -> Result<usize, InstallError>
+) -> Result<usize, DownloadError>
 where
     F: Fn(f64) + Sync + Send,
     F2: Fn(usize) + Send + Sync,
 {
-    let client = Client::builder().user_agent("deploykit").build()?;
+    let client = Client::builder()
+        .user_agent("deploykit")
+        .build()
+        .context(BuildDownloadClientSnafu)?;
 
     let head = client
         .head(&url)
         .send()
         .await
-        .and_then(|x| x.error_for_status())?;
+        .and_then(|x| x.error_for_status())
+        .context(SendRequestSnafu)?;
 
     let total_size = head
         .headers()
@@ -108,16 +150,14 @@ where
 
     let mut file = tokio::fs::File::create(&path)
         .await
-        .map_err(|e| InstallError::OperateFile {
-            path: path.display().to_string(),
-            err: e,
-        })?;
+        .context(CreateFileSnafu { path: path.clone() })?;
 
     let mut resp = client
         .get(url)
         .send()
         .await
-        .and_then(|x| x.error_for_status())?;
+        .and_then(|x| x.error_for_status())
+        .context(SendRequestSnafu)?;
 
     let mut v = Sha256::new();
 
@@ -125,7 +165,11 @@ where
     let mut v_download_len = 0;
     let mut download_len = 0;
 
-    while let Some(chunk) = resp.chunk().await? {
+    while let Some(chunk) = resp
+        .chunk()
+        .await
+        .context(DownloadFileSnafu { path: path.clone() })?
+    {
         if now.elapsed().as_secs() >= 1 {
             now = Instant::now();
             velocity(v_download_len / 1024);
@@ -138,10 +182,7 @@ where
 
         file.write_all(&chunk)
             .await
-            .map_err(|e| InstallError::OperateFile {
-                path: path.display().to_string(),
-                err: e,
-            })?;
+            .context(WriteFileSnafu { path: path.clone() })?;
 
         progress((download_len as f64 / total_size as f64 * 100.0).round());
         v.update(&chunk);
@@ -151,10 +192,7 @@ where
 
     tokio::task::spawn_blocking(move || {
         let download_hash = v.finalize().to_vec();
-
-        if hex::encode(download_hash) != hash {
-            return Err(InstallError::ChecksumMisMatch);
-        }
+        ensure!(hex::encode(download_hash) == hash, ChecksumMismatchSnafu);
 
         Ok(())
     })
@@ -163,10 +201,7 @@ where
 
     file.shutdown()
         .await
-        .map_err(|e| InstallError::OperateFile {
-            path: path.display().to_string(),
-            err: e,
-        })?;
+        .context(ShutdownFileSnafu { path: path.clone() })?;
 
     Ok(total_size)
 }
