191f0fc7755b0250a3d3543570b1db10e5bd53b6
eatradish <sakiiily@aosc.io>
2024-06-06 21:52:58 +0800

    Return Error if `sync_and_reboot` can go to return value step

--- install/src/lib.rs
+++ install/src/lib.rs
@@ -990,5 +990,8 @@ pub fn sync_and_reboot() -> io::Result<()> {
     // (https://man7.org/linux/man-pages/man2/reboot.2.html)
     unsafe { libc::reboot(LINUX_REBOOT_CMD_RESTART2) };
 
-    Ok(())
+    Err(io::Error::new(
+        io::ErrorKind::Other,
+        "Failed to reboot system",
+    ))
 }
