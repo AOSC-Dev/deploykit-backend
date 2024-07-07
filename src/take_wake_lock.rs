5dea196ce833bf8e897afa9ac59fb1e8ddcac0d3
eatradish <sakiiily@aosc.io>
2024-07-07 23:57:49 +0800

    fix: do not block shutdown

--- src/take_wake_lock.rs
+++ src/take_wake_lock.rs
@@ -3,19 +3,14 @@ use logind_zbus::manager::{InhibitType, ManagerProxy};
 use tracing::info;
 use zbus::{zvariant::OwnedFd, Connection};
 
-pub async fn take_wake_lock(conn: &Connection) -> Result<Vec<OwnedFd>> {
+pub async fn take_wake_lock(conn: &Connection) -> Result<OwnedFd> {
     let proxy = ManagerProxy::new(conn).await?;
 
-    let mut fds = Vec::new();
-    for what in [InhibitType::Shutdown, InhibitType::Sleep] {
-        let fd = proxy
-            .inhibit(what, "Deploykit", "Deploykit Installing system", "block")
-            .await?;
+    let fd = proxy
+        .inhibit(InhibitType::Sleep, "Deploykit", "Deploykit Installing system", "block")
+        .await?;
 
-        fds.push(fd);
-    }
+    info!("take wake lock: {:?}", fd);
 
-    info!("take wake lock: {:?}", fds);
-
-    Ok(fds)
+    Ok(fd)
 }
