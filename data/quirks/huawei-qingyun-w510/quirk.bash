e86e8817bbcef31cfcf07d4c72e25ed2bafbef19
Mingcong Bai <jeffbai@aosc.io>
2025-11-14 20:44:30 +0800

    chore(quirks): add commenting and error handling to scripts

--- data/quirks/huawei-qingyun-w510/quirk.bash
+++ data/quirks/huawei-qingyun-w510/quirk.bash
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+# Enable error trapping.
+set -Eeuo pipefail
+
+# Huawei Qingyun W510 (some models, such as the D1041 revision) is known
+# to shutdown between 60 and 240 seconds post-boot if the bootloader ID is
+# not set to "ubuntu". Strange.
 grub-install \
 	--force-extra-removable \
 	--bootloader-id="ubuntu"
+
+# Update GRUB.
 update-grub
