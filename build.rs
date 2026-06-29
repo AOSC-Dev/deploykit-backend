beaa798a7a39345c5f82286ebe8c139f59dac295
eatradish <sakiiily@aosc.io>
2026-06-29 11:18:20 +0800

    v0.9.2

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
 use std::error::Error;
 use vergen::Emitter;
-use vergen_gix::GixBuilder;
+use vergen_gix::Gix;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Emit the instructions
-    let gix = GixBuilder::all_git()?;
+    let gix = Gix::all_git();
     Emitter::default().add_instructions(&gix)?.emit()?;
 
     Ok(())
